e3d6e9a6bef30d17f6d6943f7c171d89b560db07
NataliaRamirez <nataliabelen6374@gmail.com>
2023-03-04 00:02:23 -0300

    url

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import store from "./redux/store/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
-axios.defaults.baseURL = `https://deploy-production-1b77.up.railway.app/`;
+axios.defaults.baseURL = `https://deploy-three-tau.vercel.app/`;
 
 ReactDOM.render(
   <Provider store={store}>
